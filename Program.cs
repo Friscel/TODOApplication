9ff4b1cd2ef19104de92702f19a9f51b9c46a68a
Friscel <182411462+Friscel@users.noreply.github.com>
2025-04-30 12:51:13 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TODOApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
